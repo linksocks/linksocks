703a6d2d6ffb76c486c7a015edcc06c60bf19ad8
jackzzs <jackzzs@outlook.com>
2025-09-14 07:59:44 +0800

    Bump version: 1.7.1 → 1.7.2

--- _bindings/python/linksocks/__init__.py
+++ _bindings/python/linksocks/__init__.py
@@ -1,6 +1,6 @@
 """linksocks: SOCKS5 over WebSocket proxy library."""
 
-__version__ = "1.7.1"
+__version__ = "1.7.2"
 
 from ._server import Server
 from ._client import Client
--- linksocks/version.go
+++ linksocks/version.go
@@ -3,6 +3,6 @@ package linksocks
 import "runtime"
 
 var (
-	Version  = "v1.7.1"
+	Version  = "v1.7.2"
 	Platform = runtime.GOOS + "/" + runtime.GOARCH
 )
